24f6e5a26ba9b48b5a79be1b479f063845d67e36
taymaa <taymaa.2917@gmail.com>
2019-11-20 15:45:04 +0200

    add app to heruko

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/18f7272513c7c4aa6c8948c4ca50ef0a/' + latitude + ',' + longitude + '?lang=bg';
+    const url = 'https://api.darksky.net/forecast/18f7272513c7c4aa6c8948c4ca50ef0a/' + latitude + ',' + longitude;
 
     request({ url, json: true }, function(err, { body }) { //json:true parser the response body as Json no need to parse it ...
         //const data = JSON.parse(res.body); becouse of json:true
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Poorly formatted request', undefined);
         } else {
-            callback(undefined, 'its ' + body.currently.icon + '. its currently ' + body.currently.temperature + ' degree out. ther is a ' + body.currently.precipProbability + '% vchance to rain');
+            callback(undefined, 'its ' + body.daily.data[0].summary + '. its currently ' + body.currently.temperature + ' degree out. ther is a ' + body.currently.precipProbability + '% vchance to rain');
         }
 
     });
